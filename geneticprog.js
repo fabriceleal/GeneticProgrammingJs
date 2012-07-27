1ccaffc8b07e01a4a72930acc896de8d8c3ab792
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-07-27 05:31:05 -0700

    organization

--- geneticprog.js
+++ geneticprog.js
@@ -1,55 +1,57 @@
 /*
 	Based in https://gist.github.com/1335696, a simple genetic programming test bed in clojure
-
 */
 
 
 (function(){
 
-	// Generate code with maxDepth / extern
-	var randomCode = function(maxDepth){
+	var Evolver = function(generatorConfig, populationConfig, population ){
 
+		// Generate code with maxDepth / extern (generator)
+		var randomCode = generatorConfig.randomCode;
+
+		// Code size of a tree / extern (generator)
+		var codesize 	= generatorConfig.codeSize;
+
+		// Inject code into individual / extern (generator)
+		var inject 		= generatorConfig.inject;
+
+		// Extract code from individual / extern (generator)
+		var extract		= generatorConfig.extract;
+
+
+		// Error function / extern (population)
+		var error		= populationConfig.error;
+
+
+		// Genetic Programming Framework
+		var mutate = function(ind){
+			return inject(randomCode(MAX_DEPTH), ind);
+		};
+
+		var crossover = function(a, b){
+			return inject(extract(a), b);
+		};
+
+
+		// "select" (TODO make a enum ...)
+		var tournament = function( inds ){
+			
+		};
+
+		// "evolve" TODO
+		this.iterate = function( inds ){
+			
+		};
+
+		// TODO Getters for these
+		var lastPopulation = population;
+		var iteration = 0;
+
+		return this;
 	};
 
-	// Code size of a tree / extern
-	var codesize = function(code){
+	// TODO func for empty configs and empty individual (?) ...
 
-	};
-	
-	// Inject code into individual / extern
-	var inject = function(code, ind){
-
-	};
-
-	// Extract code from individual / extern
-	var extract = function(ind){
-
-	};
-
-	// Error function / extern
-	var error = function(ind){
-
-	};
-	
-
-	var mutate = function(ind){
-		return inject(randomCode(MAX_DEPTH), ind);
-	};
-
-	var crossover = function(mary, joseph){
-		return inject(extract(joseph), mary);
-	};
-
-
-	// "select" (TODO make a enum ...)
-	var tournament = function(){
-		
-	};
-
-	// "evolve" TODO
-	// YOU are responsible for generating the population!
-	var iterate = function(population){
-		
-	};
-
+	exports.Evolver = Evolver;
 })();
