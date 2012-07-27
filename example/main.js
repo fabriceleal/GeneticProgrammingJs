b52a96618c0a51616257f6002ec712c1fb7073b0
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-07-27 15:32:44 -0700

    minimal working! :D :D :D

--- example/main.js
+++ example/main.js
@@ -1,5 +1,34 @@
 #!/usr/local/bin/node
 
 with(require("../geneticprog.js")){
+	var population = null;
+
+	var g = new (require("../../jsrandomproggen/madmonkey.js").Generator)('_->number');
+
+	g.addForm('(Math.sin)', 												'(number)->number' ).
+			addForm('(Math.cos)', 											'(number)->number' ).
+			addForm('(Math.sqrt)', 											'(number)->number' ).
+			addForm('(Math.tan)', 											'(number)->number' ).
+			addForm('(150)', 													'number');
+
+	population = [g.gen(10), g.gen(10), g.gen(10)];
+	//console.log(JSON.stringify(population, null, 3));
+
+	var e = new Evolver(
+			{
+				randomCode : function(depth){ return g.gen(depth); },
+				inject : function(t1, t2){ return t1.inject(t2); },
+				extract: function(t){ return t.extract(); },
+				maxDepth: 10
+			},
+			{
+				error : (function(){
+								var target = Math.sin(Math.cos(Math.sin(Math.cos(150)))); 
+								return function(i){ return eval(i.compile()) - target } 
+							})()
+			}, 
+			population);
+	//--
+	console.log(JSON.stringify(e.iterate(), null, 3));
 	
 }
--- geneticprog.js
+++ geneticprog.js
@@ -11,7 +11,7 @@
 		var randomCode = generatorConfig.randomCode;
 
 		// Code size of a tree / extern (generator)
-		var codesize 	= generatorConfig.codeSize;
+		//var codesize 	= generatorConfig.codeSize;
 
 		// Inject code into individual / extern (generator)
 		var inject 		= generatorConfig.inject;
@@ -26,7 +26,7 @@
 
 		// Genetic Programming Framework
 		var mutate = function(ind){
-			return inject(randomCode(MAX_DEPTH), ind);
+			return inject(randomCode(generatorConfig.maxDepth), ind);
 		};
 
 		var crossover = function(a, b){
@@ -34,14 +34,33 @@
 		};
 
 
-		// "select" (TODO make a enum ...)
-		var tournament = function( inds ){
-			
+		// "select"
+		var tournament = function( sortedInds, size ){
+			// Generate *size* random numbers, from 0 to sortedInds.length
+			var idx = [];
+			for(var i = 0; i < size; ++i) 
+				idx.push(Math.floor(Math.random() * sortedInds.length));
+
+			// Take the min			
+			// Take the individual at position idx
+			return sortedInds[ idx.reduce(function(t, i){ return Math.min(t, i); }, sortedInds.length)];
 		};
 
-		// "evolve" TODO
-		this.iterate = function( inds ){
+		// "evolve"
+		this.iterate = function( iterations ){
+			var sorted 	= population.sort(function(a, b){ return error(a) - error(b)});
+			var best 	= sorted[0];
+			var bestError = error(best);
 			
+			if(iterations > 0){
+
+			}
+			
+			return {
+				best : best.compile(),
+				bestError : bestError,
+				results: population.map(function(e){ return { individual:e.compile(), error:error(e) }})
+			}
 		};
 
 		// TODO Getters for these
